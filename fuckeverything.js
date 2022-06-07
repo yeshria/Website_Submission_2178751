85afcbcffe96c0ab82a127c22da7e81989e6de0b
Yeshria <yeshria.pillay@gmail.com>
2022-06-08 00:38:16 +0200

    Push 1 : 08/06/2022
    Got navigation JavaScript working.
    Added responsiveness for blog page, blog posts, and design page.
    What is left:
    Homepage, projects page, and skills page responsiveness.
    Microformats.

--- fuckeverything.js
+++ fuckeverything.js
@@ -7,29 +7,51 @@
 
 (function()
 {
-    var navItems = [{href: '../index.html', text: "home"},{href: "../Pages/projs.html", text: 'projs'},{href: '../Pages/random.html', text: 'random'}];
+    var navItems = [{href: '../index.html', text: 'Home'},{href: '../Pages/projs.html', text: 'Projs'},{href: '../Pages/random.html', text: 'Skills'}];
 
 
    var oooElem = document.createElement("nav");
+   document.body.appendChild(oooElem);
+   console.log(oooElem);
+
    var uList = document.createElement("ul");
-   var newItem;
-   var newLink;
-  
-    oooElem.appendChild(uList);
 
-    for (var i = 0; i < uList.length(); i++) 
-        {
-        newItem = document.createElement("li");
-        newLink = document.createElement("a");
-  
-        newLink.href = navItems[i].href;
-        newLink.innerHTML = navItems[i].text;
-  
-        newItem.appendChild(newLink);
-        uList.appendChild(newItem);
+   uList = oooElem.appendChild(uList);
 
-        }
-        // uList.children[0].className = "current";
+   var newItem = document.createElement("li");
+    newItem = uList.appendChild(newItem);
+   var newLink = document.createElement("a");
+   newLink.href = '../index.html';
+   newLink.innerHTML = 'home';
+   newLink = uList.appendChild(newLink);
+   //newItem.appendChild(newLink);
+
+
+
+     //alert("in function");
+
+   //  for (var i = 0; i < uList.length; i++) 
+   //      {
+   //      newItem = document.createElement("li");
+   //      // uList.appendChild(newItem);
+   //      // newItem.appendChild(newLink);
+
+   //      newLink = document.createElement("a");
+   //      uList.appendChild(newLink);
+    
+   //      newLink.body.href = navItems[i].href;
+   //      newLink.body.innerHTML = navItems[i].text;
+
+   //      uList.appendChild(newLink);
+  
+   //      // newItem.appendChild(newLink);
+   //      uList.appendChild(newItem);
+   //      // alert("in loop");
+
+   //      }
+
+      //   console.log(uList.children);
+      //    uList.children[0].classList.add("current");
 
       // callin function
          window.onload = function() 
@@ -38,3 +60,5 @@
              }
   
     }());
+
+
---
+++ tumi.js
@@ -0,0 +1,54 @@
+// window.onload = function () 
+// {
+//     document.body.appendChild(FuckingElem);
+// }
+
+
+
+(function()
+{
+    var navItems = [{href: '../index.html', text: 'Home'},{href: '../Pages/projs.html', text: 'Blogs'},{href: '../Pages/random.html', text: 'Skills'},{href: '../Pages/spoopy.html', text: 'Design'}, {href: '../Pages/contact.html', text: 'Projects'}];
+    var uList, oooElem, newItem, newLink;
+
+   
+   //newItem.appendChild(newLink);
+
+
+
+     //alert("in function");
+
+    for (var i = 0; i < navItems.length; i++) 
+        {
+            
+            var oooElem = document.createElement("nav");
+            oooElem.classList = "current";
+            document.body.appendChild(oooElem);
+
+            console.log(oooElem);
+         
+            var uList = document.createElement("ul");
+         
+            uList = oooElem.appendChild(uList);
+         
+            var newItem = document.createElement("li");
+             newItem = uList.appendChild(newItem);
+            var newLink = document.createElement("a");
+            newLink.href = navItems[i].href;
+            newLink.innerHTML = navItems[i].text;
+            newLink = uList.appendChild(newLink);
+
+        }
+
+        oooElem.children[0].class.add("current");
+      //   console.log(uList.children);
+         
+
+      // callin function
+         window.onload = function() 
+             {
+                document.body.appendChild(oooElem);
+             }
+  
+    }());
+
+
